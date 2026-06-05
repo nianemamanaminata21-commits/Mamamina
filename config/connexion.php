f46e82da438555ace53411b3b2749253d061fb49
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-06-05 12:31:11 +0000

    ajout compte prof blog , correction css

--- config/connexion.php
+++ config/connexion.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'portfolio');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'sql209.infinityfree.com');
+define('DB_NAME', 'if0_41918765_portfolio');
+define('DB_USER', 'if0_41918765_root');
+define('DB_PASS', 'Nanouche14');
 try{
     $pdo=new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
