2046d6e21e27e24b2fb9d1816d17a59d506156e2
Maman Aminata Niane <nianemamanaminata21@gmail.com>
2026-06-05 20:40:06 +0000

    ajout

--- config/connexion.php
+++ config/connexion.php
@@ -8,7 +8,6 @@ try{
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
-    error_log($e->getMessage());
-    die("Erreur de connexion à la base de données.");
+    die($e->getMessage());
 }
 ?>
